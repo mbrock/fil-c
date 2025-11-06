0a950435c861ba4d40d5e093f7c4abbec10fbff2
Mikael Brockman <mikael@brockman.se>
2025-11-06 16:24:20 +0200

    pcc's fix for #90

--- llvm/lib/Transforms/Instrumentation/FilPizlonator.cpp
+++ llvm/lib/Transforms/Instrumentation/FilPizlonator.cpp
@@ -8862,7 +8862,7 @@ class Pizlonator {
           }
           continue;
         }
-        if (Tok.Str == ".filc_symver") {
+        if (Tok.Str == ".filc_symver" || Tok.Str == ".symver") {
           std::string LocalName = MAT.getNextSpecific(MATokenKind::Identifier).Str;
           MAT.getNextSpecific(MATokenKind::Comma);
           std::string VersionedName = MAT.getNextSpecific(MATokenKind::Identifier).Str;
